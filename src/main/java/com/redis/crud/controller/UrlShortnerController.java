5d433e053b23e74a3ab11707be9ccd14bb09a2f2
Mohanad <mohanadkhaled87@gmail.com>
2024-09-24 14:56:56 +0300

    Added logger to controllers,services

--- src/main/java/com/redis/crud/controller/UrlShortnerController.java
+++ src/main/java/com/redis/crud/controller/UrlShortnerController.java
@@ -6,11 +6,15 @@ import jakarta.validation.Valid;
 import org.hibernate.validator.constraints.URL;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping("/public/api/url")
 public class UrlShortnerController {
 
     private UrlShortener urlShortener;
+    private final Logger logger = Logger.getLogger(UrlShortnerController.class.getName());
 
     public UrlShortnerController(UrlShortener urlShortener) {
         this.urlShortener = urlShortener;
@@ -18,11 +22,13 @@ public class UrlShortnerController {
 
     @GetMapping("/{id}")
     public String getById(@PathVariable String id){
+        logger.info("Sending {" + id + "} to shortener service");
         return urlShortener.getUrl(id);
     }
 
     @PostMapping("/")
     public String addUrl(@RequestBody @Valid RequestUrlDto requestUrlDto){
+        logger.info("Sending {" + requestUrlDto.getUrl() + "to shortener service");
         return urlShortener.createUrl(requestUrlDto.getUrl());
     }
 
--- src/main/java/com/redis/crud/service/UrlShortener.java
+++ src/main/java/com/redis/crud/service/UrlShortener.java
@@ -5,23 +5,29 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
+import java.util.logging.Logger;
 
 @Service
 public class UrlShortener {
 
     StringRedisTemplate redisTemplate;
 
+    private final Logger logger = Logger.getLogger(UrlShortener.class.getName());
+
     public UrlShortener(StringRedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
 
     public String getUrl(String id){
+        logger.info("Searching for the url by its key in redis");
         String url = redisTemplate.opsForValue().get(id);
         return url;
     }
 
     public String createUrl(String url){
+        logger.info("Creating new hashing for {" + url + "}");
         String id = Hashing.sha256().hashString(url, StandardCharsets.UTF_8).toString();
+        logger.info("Saving the hashed url in redis");
         redisTemplate.opsForValue().set(id, url);
         return id;
     }
